3cf370b8a70666e25ed33b7fca9f3dff2dd0ef68
AipioxTechson <ritvik.bhardwaj@outlook.com>
2019-07-28 18:25:12 -0400

    Add Boilerplate Code
    Adds basecode for the different classes needed

--- Game.py
+++ Game.py
@@ -1,4 +1,27 @@
 class Game:
-    def __init__(self,total_pawns,player1,player2):
-        self.grid = []
+    """Responsible for holding the state of the Game.
+    
+    Functions:
+    isWin()
+    nextMove()
+    printState()
+    
+    """
+
+    def __init__(self,player1: "Player" ,player2: "Player", grid = []):
+        self.grid = grid
+        self.turn = 0
+        self.player_one = player1
+        self.player_two = player2
+    
+    def isWin(self):
+        pass
+    
+    def nextMove(self):
+        pass
+    
+    def printState(self):
+        pass
+                
+            
         
--- GameController.py
+++ GameController.py
@@ -1,1 +1,25 @@
 from Game import *
+from Players import *
+
+def generateGrid(size,
+print("Welcome to nPawn!")
+print("How many Pawns?")
+try:
+    total = int(input())
+except ValueError:
+    total = 3
+
+if total < 2:
+    total = 3
+    
+print("What gamemode?: (1) PvP (2) PvC")
+try:
+    gamemode = int(input())
+except ValueError:
+    gamemode = 1
+    
+if gamemode < 0 or gamemode >= 3:
+    gamemode = 1
+    
+    
+player1, player2 = None, None
